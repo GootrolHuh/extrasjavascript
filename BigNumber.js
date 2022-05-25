cbd6c724828a90a26139b16ae2ff877caa57b1a2
GootrolHuh <105822763+GootrolHuh@users.noreply.github.com>
2022-05-25 20:54:32 -0300

    Add files via upload

---
+++ BigNumber.js
@@ -0,0 +1,111 @@
+function BigNumberOne() {
+    return 1;
+}
+
+function BigNumberTwo() {
+    return 2;
+}
+
+function BigNumberThree() {
+    return 3;
+}
+
+function BigNumberFour() {
+    return 4;
+}
+
+function BigNumberFive() {
+    return 5;
+}
+
+function BigNumberSix() {
+    return 6;
+}
+
+function BigNumberSeven() {
+    return 7;
+}
+
+function BigNumberEight() {
+    return 8;
+}
+
+function BigNumberNine() {
+    return 9;
+}
+
+function BigNumberTen() {
+    return 10;
+}
+
+function BigNumberHundred() {
+    return 100;
+}
+
+function BigNumberThousand() {
+    return 1000;
+}
+
+function BigNumberMillion() {
+    return 1000000;
+}
+
+function BigNumberTwoPower(pow) {
+    return 2 ** pow;
+}
+
+function BigNumberThreePower(pow) {
+    return 3 ** pow;
+}
+
+function BigNumberFourPower(pow) {
+    return 4 ** pow;
+}
+
+function BigNumberFivePower(pow) {
+    return 5 ** pow;
+}
+
+function BigNumberSixPower(pow) {
+    return 6 ** pow;
+}
+
+function BigNumberSevenPower(pow) {
+    return 7 ** pow;
+}
+
+function BigNumberEightPower(pow) {
+    return 8 ** pow;
+}
+
+function BigNumberNinePower(pow) {
+    return 9 ** pow;
+}
+
+function BigNumberTenPower(pow) {
+    return 10 ** pow;
+}
+
+function BigNumberHundredPower(pow) {
+    return 100 ** pow;
+}
+
+function BigNumberThousandPower(pow) {
+    return 1000 ** pow;
+}
+
+function BigNumberSelect(number) {
+    return number;
+}
+
+function BigNumberSelectPower(number, pow) {
+    return number ** pow;
+}
+
+function BigNumberPi() {
+    return Math.PI
+}
+
+function BigNumberPiplex() {
+    return 10 ** Math.PI
+}
